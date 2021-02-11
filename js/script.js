2f1cf9b8d21df3e5f4906e8439e275579a3bfeab
Federico <crudofederico@gmail.com>
2021-02-11 12:37:08 +0100

    update

--- js/script.js
+++ js/script.js
@@ -3,10 +3,11 @@ $(document).ready(function(){
     $("#search").click(function(){
         $('.search-box').slideToggle();
     });
-    $("windows").scroll(function(){
-        $('nav-bar').toggleClass('scroll',this.scrollTop())
-    });
-
+  
+    window.addEventListener('scroll',function(){
+        let header=document.querySelector('.nav-bar');
+        header.classList.toggle('scroll',window.scrollY >0)
+    })
 
 
   });
@@ -19,9 +20,14 @@ var app = new Vue({
       apikey:"b7ccd3b571de65818efd8d90d551616e",
       lang:"it-IT",
       searchElement:[],
+      generi:[],
+      popular:[],
+      mostpopular:'',  
     },
     methods:{
         search(){
+               
+            
                axios
         .get('https://api.themoviedb.org/3/search/multi',{
             params:{
@@ -37,7 +43,37 @@ var app = new Vue({
            
         })
         .catch(error => console.log('errore'));
-        }
+        },
+        clear(){
+           (this.query.length==0)?this.searchElement=[] :'';
+            
+        },
+       
+
    
+    },
+    mounted() { 
+         axios
+            .get('https://api.themoviedb.org/3/trending/all/week',{
+                params:{
+                    api_key: this.apikey,
+                    language:this.lang,
+                }
+            })
+            .then((result) => {
+                let a=0;
+               this.popular=result.data.results;
+               console.log(this.popular);
+               this.popular.forEach(element => {
+                   if(element.popularity>a){
+                       a=element.popularity;
+                       this.mostpopular=element;
+                   }
+                   
+               });
+               console.log(this.mostpopular);
+               
+            })
+            .catch(error => console.log('errore'));
     }
   });
